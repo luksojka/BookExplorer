2a6319f62e4a9622d90373adc1137d494b8e4e82
Łukasz Sójka <lsojka93@gmail.com>
2024-03-09 16:24:40 +0100

    FindByAuthor method

--- src/main/java/com/project/bookexplorer/ApplicationStartup.java
+++ src/main/java/com/project/bookexplorer/ApplicationStartup.java
@@ -13,18 +13,23 @@ public class ApplicationStartup implements CommandLineRunner {
 
     private final CatalogController catalogController;
     private final String title;
+    private final String author;
 
     public ApplicationStartup(
             CatalogController catalogController,
-            @Value("${bookexplorer.catalog.query}") String title
+            @Value("${bookexplorer.catalog.title}") String title,
+            @Value("${bookexplorer.catalog.author}") String author
     ){
     this.catalogController = catalogController;
     this.title = title;
+    this.author = author;
     }
 
     @Override
     public void run(String... args){
         List<Book> books = catalogController.findByTitle(title);
+        List<Book> books2 = catalogController.findByAuthor(author);
         books.forEach(System.out::println);
+        books2.forEach(System.out::println);
     }
 }
--- src/main/java/com/project/bookexplorer/catalog/domain/CatalogService.java
+++ src/main/java/com/project/bookexplorer/catalog/domain/CatalogService.java
@@ -17,8 +17,15 @@ public class CatalogService {
 
     public List<Book> findByTitle(String title) {
         return repository.findAll()
-                        .stream()
-                        .filter(book -> book.getTitle().startsWith(title))
-                        .collect(Collectors.toList());
+                .stream()
+                .filter(book -> book.getTitle().startsWith(title))
+                .collect(Collectors.toList());
+    }
+
+    public List<Book> findByAuthor(String author) {
+        return repository.findAll()
+                .stream()
+                .filter(book -> book.getAuthor().startsWith(author))
+                .collect(Collectors.toList());
     }
 }
