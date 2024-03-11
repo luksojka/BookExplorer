db4a10e4665af9da623208b5c688c1488c26d881
Łukasz Sójka <lsojka93@gmail.com>
2024-03-11 22:48:46 +0100

    Data management amendment - data generation during startup of application. Deletion of unnecessary repository.

--- src/main/java/com/project/bookexplorer/catalog/infrastructure/MemoryCatalogRepository.java
+++ src/main/java/com/project/bookexplorer/catalog/infrastructure/MemoryCatalogRepository.java
@@ -9,23 +9,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 @Primary
-class BestsellerCatalogRepository implements CatalogRepository {
+class MemoryCatalogRepository implements CatalogRepository {
 
     private final Map<Long, Book> storage = new ConcurrentHashMap<>();
-
-    public BestsellerCatalogRepository() {
-        storage.put(1L, new Book(1L, "Diune", "Frank Herbert", 1965));
-        storage.put(2L, new Book(2L, "Okrakiem przez Atlantyk", "Andrzej Radomiński", 1982));
-        storage.put(3L, new Book(3L, "The Hobbit", "J.R.R. Tolkien", 1960));
-        storage.put(4L, new Book(4L, "Harry Potter and the Sorcerer's Stone", "J. K. Rowling", 1997));
-    }
+    private final AtomicLong ID_NEXT_VALUE = new AtomicLong(0L);
 
     @Override
     public List<Book> findAll() {
         return new ArrayList<>(storage.values());
+    }
+
+    @Override
+    public void save(Book book) {
+        long nextId = nextId();
+        book.setId(nextId);
+        storage.put(nextId, book);
+    }
+
+    private long nextId() {
+        return ID_NEXT_VALUE.getAndIncrement();
     }
 
 }
